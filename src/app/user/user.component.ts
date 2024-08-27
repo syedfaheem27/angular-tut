78266fbc34aabca3bc714976dcdf35355b830e99
syedfaheem27 <syedfaheem2719@gmail.com>
2024-08-27 16:48:53 +0530

    Add component inputs using the @input decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,4 @@
-import { Component, signal, computed, WritableSignal } from '@angular/core';
-import { user } from '../interfaces/user';
-import { DUMMY_USERS } from '../dummy-users';
-
-const generateRandomIndex = () =>
-  Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -13,25 +8,12 @@ const generateRandomIndex = () =>
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser: WritableSignal<user> = signal(
-    DUMMY_USERS[generateRandomIndex()]
-  );
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  //when the selected user changes, someValue won't change
-  //that's why we will use computed to properly observe the
-  //selectedUser and update someValue when it changes
-
-  // someValue = this.selectedUser().avatar;
-
-  // someValue = computed(() => this.selectedUser().avatar);
-
-  // get imagePath() {
-  //   return `assets/users/${this.selectedUser().avatar}`;
-  // }
-
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
-
-  onUserClick() {
-    this.selectedUser.set(DUMMY_USERS[generateRandomIndex()]);
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
   }
+
+  onUserClick() {}
 }
